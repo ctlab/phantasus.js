a32331db3e6f5101a7101c0ce60a445a8027e101
petrovich <mazahakaforever@ya.ru>
2018-07-25 10:34:39 +0300

    GSEA heatmap mark 1

--- src/tools/gsea_tool.js
+++ src/tools/gsea_tool.js
@@ -17,8 +17,7 @@ phantasus.gseaTool = function (project) {
   this.$el = $('<div class="container-fluid" style="height: 100%">'
     + '<div class="row" style="height: 100%">'
     + '<div data-name="configPane" class="col-xs-2"></div>'
-    + '<div class="col-xs-10" style="height: 100%">'
-    + '   <div style="position:relative; height: 100%;" data-name="chartDiv"></div>'
+    + '<div class="col-xs-10" id="draw-place-holder" style="height: 100%">'
     + '</div>'
     + '</div></div>');
 
@@ -36,6 +35,7 @@ phantasus.gseaTool = function (project) {
   }, {
     name: 'vertical',
     type: 'checkbox',
+    tooltipHelp: 'Places plot vertically and draws heatmap of selected dataset',
     value: false
   }/*, {
     name: 'chart_width',
@@ -60,28 +60,34 @@ phantasus.gseaTool = function (project) {
     var fullDataset = project.getSortedFilteredDataset();
     $notifyRow.toggle(selectedDataset.getRowCount() === fullDataset.getRowCount());
 
-    if (selectedDataset.getRowCount() === fullDataset.getRowCount()) {
-      return;
-    }
-
     if (self.promise) {
       self.promise.reject('Cancelled');
     }
 
-    self.request(project).then(self.draw.bind(self), function (e) {
-      self.$chart.empty();
-    });
+    if (selectedDataset.getRowCount() === fullDataset.getRowCount()) {
+      return;
+    }
+
+    self.promise = self.request(project);
+    self.promise.then(self.draw.bind(self));
+  }, 500);
+
+  var updateHeatMap = _.debounce(function () {
+    self.drawHeatmap();
   }, 500);
 
   this.formBuilder.$form.on('change', 'select', onChange);
   this.formBuilder.$form.on('change', 'input', onChange);
   project.getRowSelectionModel().on('selectionChanged.chart', onChange);
+  project.on(phantasus.Project.Events.ROW_SORT_ORDER_CHANGED, updateHeatMap);
+  project.on(phantasus.Project.Events.COLUMN_SORT_ORDER_CHANGED, updateHeatMap);
 
+  this.project = project;
 
   var $configPane = this.$el.find('[data-name=configPane]');
   this.formBuilder.$form.appendTo($configPane);
   this.$el.appendTo(this.$dialog);
-  this.$chart = this.$el.find("[data-name=chartDiv]");
+  this.$drawPlaceHolder = this.$el.find('#draw-place-holder');
   this.$dialog.dialog({
     open: function (event, ui) {
       $(this).css('overflow', 'hidden'); //this line does the actual hiding
@@ -91,8 +97,11 @@ phantasus.gseaTool = function (project) {
       self.$dialog.dialog('destroy').remove();
       event.stopPropagation();
     },
+    /*resize: function () { //Don't let user change dialog screen for now
+      updateHeatMap();
+    },*/
 
-    resizable: true,
+    resizable: false,
     height: 600,
     width: 900
   });
@@ -105,17 +114,17 @@ phantasus.gseaTool.prototype = {
     return 'gsea Plot';
   },
   request: function (project) {
-    this.$chart.empty();
-    phantasus.Util.createLoadingEl().appendTo(this.$chart);
-    this.promise = $.Deferred();
+    this.$drawPlaceHolder.empty();
+    phantasus.Util.createLoadingEl().appendTo(this.$drawPlaceHolder);
+    var promise = $.Deferred();
 
     var selectedDataset = project.getSelectedDataset();
     var fullDataset = project.getFullDataset();
 
     if (selectedDataset.getRowCount() === fullDataset.getRowCount()) {
-      this.promise.reject('Invalid rows');
+      promise.reject('Invalid rows');
       // throw new Error('Invalid amount of rows are selected (zero rows or whole dataset selected)');
-        return this.promise;
+      return promise;
     }
 
     var idxs = selectedDataset.rowIndices.map(function (idx) {
@@ -149,20 +158,91 @@ phantasus.gseaTool.prototype = {
           var absolutePath = phantasus.Util.getFilePath(session, svgPath);
           phantasus.BlobFromPath.getFileObject(absolutePath, function (blob) {
             self.imageURL = URL.createObjectURL(blob);
-            self.promise.resolve(self.imageURL);
+            promise.resolve(self.imageURL);
           });
         });
       }, false, "::" + fullDataset.getESVariable())
         .fail(function () {
-          self.promise.reject();
+          promise.reject();
         });
-    })
+    });
 
-    return self.promise;
+    return promise;
   },
   draw: function (url) {
-    this.$chart.empty();
-    var svg = $('<img src="' + url + '" style="max-width: 100%; height: 100%; position: absolute; margin: auto; top: 0; left: 0; right: 0; bottom: 0;">');
-    svg.appendTo(this.$chart);
+    var vertical = this.formBuilder.getValue('vertical');
+    this.$drawPlaceHolder.empty();
+    var result;
+
+    if (!vertical) {
+      result = $( '<div style="position:relative; height: 100%;">' +
+                    '<img src="' + url + '" style="max-width: 100%; height: 100%; position: absolute; margin: auto; top: 0; left: 0; right: 0; bottom: 0;">' +
+                  '</div>');
+      result.appendTo(this.$drawPlaceHolder);
+    } else {
+      result = $(['<div class="col-sm-5" style="height: 100%" id="heatmap-container"></div>',
+                  '<div class="col-sm-7" style="position:relative; height: 100%;">',
+                  '   <img src="' + url + '" style="max-width: 100%; height: 100%; position: absolute; margin: auto; top: 0; left: 0; right: 0; bottom: 0;">',
+                  '</div>'].join(''));
+      result.appendTo(this.$drawPlaceHolder);
+      this.drawHeatmap();
+    }
+  },
+
+  drawHeatmap: function () {
+    var vertical = this.formBuilder.getValue('vertical');
+    if (!vertical) {
+      return;
+    }
+
+
+    var heatmapContainer = $('#heatmap-container');
+    heatmapContainer.empty();
+    var heatmap = new phantasus.HeatMapElementCanvas(this.project);
+    heatmap.setDataset(this.project.getSortedFilteredDataset());
+
+    var cs = new phantasus.HeatMapColorScheme(this.project);
+    cs.setColorSupplierForCurrentValue(phantasus.AbstractColorSupplier.fromJSON({
+      scalingMode: 'relative'
+    }));
+
+    heatmap.setColorScheme(cs);
+
+    var canvas = $('<canvas></canvas>')[0];
+    canvas.height = phantasus.CanvasUtil.BACKING_SCALE * $(heatmapContainer).height();
+    canvas.style.height = $(heatmapContainer).height() + 'px';
+    canvas.width = phantasus.CanvasUtil.BACKING_SCALE * $(heatmapContainer).width();
+    canvas.style.width = $(heatmapContainer).width() + 'px';
+    var context = canvas.getContext('2d');
+
+    var positions = heatmap.getRowPositions();
+    var totalCurrent = positions.getItemSize(positions.getLength() - 1)
+      + positions.getPosition(positions.getLength() - 1);
+
+    var size = positions.getSize();
+    size = size * ($(heatmapContainer).height() / totalCurrent);
+    size = Math.min(13, size);
+
+    positions.setSize(size);
+
+
+    var positions = heatmap.getColumnPositions();
+    var totalCurrent = positions.getItemSize(positions.getLength() - 1)
+      + positions.getPosition(positions.getLength() - 1);
+    var size = positions.getSize();
+    size = size * ($(heatmapContainer).width() / totalCurrent);
+    size = Math.min(13, size);
+    positions.setSize(size);
+
+
+    $(canvas).appendTo(heatmapContainer);
+    heatmap.draw({
+      x: 0,
+      y: 0,
+      width: $(heatmapContainer).width(),
+      height: $(heatmapContainer).height()
+    }, context);
+
+    heatmap.repaint();
   }
 };
