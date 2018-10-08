f7e66ae1916318a8c60a98d4fc51ca45082f7506
Vladislav Kamenev <mazahakaforever@ya.ru>
2018-10-08 15:45:48 +0300

    Export dataset history environment save

--- src/tools/export_dataset_history_tool.js
+++ src/tools/export_dataset_history_tool.js
@@ -2,16 +2,19 @@
 phantasus.ExportDatasetHistory = function (project) {
   var _this = this;
 
-
   this.$el = $('<div class="container-fluid">'
     + '<div class="row">'
-    + '<div data-name="controlPane" class="col-xs-2"><button id="copy_btn" class="btn btn-default btn-sm">Copy to clipboard</button></div>'
+    + '<div data-name="controlPane" class="col-xs-2">'
+      + '<button id="copy_btn" class="btn btn-default btn-sm">Copy to clipboard</button>'
+      + '<button id="save_env_btn" style="margin-top:15px; display: none" class="btn btn-default btn-sm">Save environment</button>'
+    + '</div>'
     + '<div class="col-xs-10"><div data-name="codePane"></div></div>'
     + '<div class=""></div>'
     + '</div></div>');
 
   this.codePane = this.$el.find('[data-name=codePane]');
   this.copyBtn = this.$el.find('#copy_btn');
+  this.saveEnvBtn = this.$el.find('#save_env_btn');
 
   this.copyBtn.on('click', function () {
     _this.codePane.find('textarea').select();
@@ -60,15 +63,18 @@ phantasus.ExportDatasetHistory.prototype = {
         sessionName: esSession.key,
         esVariable: dataset.getESVariable()
       }, function (newSession) {
-        newSession.getMessages(function (a) {
-          console.log(a);
-        });
         newSession.getObject(function (success) {
           var text = JSON.parse(success)[0];
 
           var textarea = $('<textarea style="height: 100%; width: 100%; resize: none;" id="codeArea" readonly>' + text + '</textarea>');
           _this.codePane.empty();
           textarea.appendTo(_this.codePane);
+
+          _this.saveEnvBtn.off();
+          _this.saveEnvBtn.on('click', function () {
+            window.open(newSession.getLoc() + 'R/env/rda', '_blank');
+          });
+          _this.saveEnvBtn.css('display', 'block');
         });
       }).fail(function () {
         throw new Error('Failed to export dataset to R. See console');
