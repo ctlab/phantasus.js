80bfea61b0f9dae8b8383d03c3bf3defc174d1ce
Vladislav Kamenev <mazahakaforever@ya.ru>
2019-01-10 17:43:17 +0300

    Draggable custom order

--- Gruntfile.js
+++ Gruntfile.js
@@ -53,7 +53,7 @@ module.exports = function (grunt) {
           'js/FileSaver.min.js', 'js/colorbrewer.js',
           'js/jquery.event.drag-2.2.js', 'js/slick.min.js', 'js/canvas-toBlob.js',
           'js/js.cookie.js', 'js/long.js', 'js/bytebuffer.js', 'js/protobuf.js',
-          'js/opencpu-0.5.js', 'js/jstat.min.js', 'js/blob-stream.js',
+          'js/opencpu-0.5.js', 'js/jstat.min.js', 'js/blob-stream.js', 'js/jquery.multisortable.js',
           'js/canvas2pdf.js', 'js/papaparse.min.js']
       },
       extJs2: {
