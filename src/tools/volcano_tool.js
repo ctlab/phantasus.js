5dd74f6eed6bbf94c347d36927593b550e68336a
Saksham Malhotra <saksham2196@gmail.com>
2019-12-09 15:24:28 +0530

    add shape and size by options

--- src/tools/volcano_tool.js
+++ src/tools/volcano_tool.js
@@ -1,9 +1,10 @@
 phantasus.volcanoTool = function (heatmap, project) {
     var self = this;
     var drawFunction = null;
+    /// var project = this.project;
 
     var fullDataset = project.getFullDataset();
-
+    self.fullDataset = fullDataset;
     //console.log('full', fullDataset.getRowMetadata());
 
     var rowMetaNames = phantasus.MetadataUtil.getMetadataNames(fullDataset.getRowMetadata());
@@ -57,6 +58,40 @@ phantasus.volcanoTool = function (heatmap, project) {
       formStyle: 'vertical'
     });
 
+    var rowOptions = []; 
+    var numericRowOptions = [];
+
+    var updateOptions = function () {
+      rowOptions = [{
+        name: "(None)",
+        value: ""
+      }];
+      numericRowOptions = [{
+        name: "(None)",
+        value: ""
+      }];
+      phantasus.MetadataUtil.getMetadataNames(fullDataset.getRowMetadata())
+      .forEach(
+        function (name) {
+          var dataType = phantasus.VectorUtil
+            .getDataType(fullDataset.getRowMetadata()
+              .getByName(name));
+          if (dataType === "number"
+            || dataType === "[number]") {
+            numericRowOptions.push({
+              name: name,
+              value: name
+            });
+          }
+          rowOptions.push({
+            name: name,
+            value: name
+          });
+        });
+      };
+
+    updateOptions();
+
     [{
       name: 'p_val_significance',
       value: '0.05',
@@ -65,6 +100,14 @@ phantasus.volcanoTool = function (heatmap, project) {
       name: 'logFC_significance',
       value: '2',
       type: 'text'
+    }, {
+      name: 'shape',
+      type: 'select',
+      options: rowOptions
+    }, {
+      name: 'size',
+      type: 'select',
+      options: numericRowOptions,
     }, {
       name: 'annotate_by',
       options: annotations,
@@ -92,17 +135,17 @@ phantasus.volcanoTool = function (heatmap, project) {
     });
     setVisibility();
 
-    var trackChanged = function () {
-      //// console.log("track changed");
-      updateOptions();
-      setVisibility();
-      formBuilder.setOptions("x-axis", pcaOptions, true);
-      formBuilder.setOptions("y-axis", pcaOptions, true);
-    };
+    // var trackChanged = function () {
+    //   //// console.log("track changed");
+    //   //updateOptions();
+    //   setVisibility();
+    //   formBuilder.setOptions("x-axis", pcaOptions, true);
+    //   formBuilder.setOptions("y-axis", pcaOptions, true);
+    // };
 
-    project.getColumnSelectionModel().on("selectionChanged.chart", trackChanged);
-    project.getRowSelectionModel().on("selectionChanged.chart", trackChanged);
-    project.on("trackChanged.chart", trackChanged);
+    // project.getColumnSelectionModel().on("selectionChanged.chart", trackChanged);
+    // project.getRowSelectionModel().on("selectionChanged.chart", trackChanged);
+    // project.on("trackChanged.chart", trackChanged);
     this.$chart = this.$el.find("[data-name=chartDiv]");
     var $dialog = $('<div style="background:white;" title="Chart"></div>');
     var $configPane = this.$el.find('[data-name=configPane]');
@@ -124,9 +167,9 @@ phantasus.volcanoTool = function (heatmap, project) {
 
     $dialog.dialog({
       close: function (event, ui) {
-        project.off('trackChanged.chart', trackChanged);
-        project.getRowSelectionModel().off('selectionChanged.chart', trackChanged);
-        project.getColumnSelectionModel().off('selectionChanged.chart', trackChanged);
+        // project.off('trackChanged.chart', trackChanged);
+        // project.getRowSelectionModel().off('selectionChanged.chart', trackChanged);
+        // project.getColumnSelectionModel().off('selectionChanged.chart', trackChanged);
         $dialog.dialog('destroy').remove();
         event.stopPropagation();
         self.volcano = null;
@@ -148,7 +191,7 @@ phantasus.volcanoTool.getPlotlyDefaults = function(){
     autosize: true,
     // paper_bgcolor: 'rgb(255,255,255)',
     // plot_bgcolor: 'rgb(229,229,229)',
-    showlegend: false,
+    showlegend: true,
     margin: {
       b: 40,
       l: 60,
@@ -197,7 +240,7 @@ phantasus.volcanoTool.getPlotlyDefaults = function(){
   };
   return {
     layout: layout,
-    config: config
+    congetSignificantfig: config
   };
 };
 
@@ -227,38 +270,130 @@ phantasus.volcanoTool.prototype = {
   },
   annotate: function() {
 
-    var _this = this
+    var _this = this;
+    console.log("inside annotate")
+    var fullDataset = _this.fullDataset;
+    console.log(_this.fullDataset)
+    /// var fullDataset = _this.project.getFullDataset();
+    var data = [];
+    var sizeBy = _this.formBuilder.getValue('size');
+    var shapeBy = _this.formBuilder.getValue('shape');
+
+    console.log("options")
+    console.log(sizeBy, shapeBy);
+
+    var sizeByVector = fullDataset.getRowMetadata().getByName(sizeBy);
+    var shapeByVector = fullDataset.getRowMetadata().getByName(shapeBy);
+
+    var size = sizeByVector ? [] : 8;
+    var shapes = shapeByVector ? [] : null;
+
+    if (sizeByVector) {
+      var minMax = phantasus.VectorUtil.getMinMax(sizeByVector);
+      console.log("MinMax", minMax);
+      var sizeFunction = d3.scale.linear()
+          .domain([minMax.min, minMax.max])
+          .range([3, 15])
+          .clamp(true);
+
+        size = _.map(phantasus.VectorUtil.toArray(sizeByVector), sizeFunction);
+      }
+
+    if (shapeByVector) {
+      var allShapes = ['circle', 'square', 'diamond', 'cross', 'triangle-up', 'star', 'hexagram', 'bowtie', 'diamond-cross', 'hourglass', 'hash-open'];
+      var uniqShapes = {};
+      shapes = _.map(phantasus.VectorUtil.toArray(shapeByVector), function (value) {
+        if (!uniqShapes[value]) {
+          uniqShapes[value] = allShapes[_.size(uniqShapes) % _.size(allShapes)];
+        }
+
+        return uniqShapes[value]
+      });
+
+      if (_.size(uniqShapes) > _.size(allShapes)) {
+        phantasus.FormBuilder.showInModal({
+          title: 'Warning',
+          html: 'Too much factors for shapes. Repeating will occur'
+        });
+      }
+
+      _.each(uniqShapes, function (shape, categoryName) {
+        data.push({
+          x: [], y: [],
+          marker: {
+            symbol: shape,
+            color: '#000000',
+            size: 6
+          },
+          name: categoryName,
+          legendgroup: 'shapes',
+          mode: "markers",
+          type: "scatter",
+          showlegend: true
+        });
+      });
+    }
+
+    data.unshift({
+      marker: {
+        color: "blue",
+        size: size,
+        symbol: shapes
+      },
+      name: "",
+      type: "scatter",
+      mode: "markers",
+      showlegend: false
+    });
+
+    data.unshift({
+      marker: {
+        color: "red",
+        size: size,
+        symbol: shapes
+      },
+      name: "",
+      mode: "markers",
+      type: "scatter",
+      showlegend: false
+    });
+
+    console.log(data);
+
     var SigObj =  _this.getSignificant(_this.plotFields[0].array, _this.plotFields[1].array)
     console.log("Sig")
 
     var logFC_a = _this.plotFields[0].array;
     var pval_a = _this.plotFields[1].array
 
-    var significant_trace = {
-      x : SigObj["sig"].map(function(i) {return(logFC_a[i])}),
-      y : SigObj["sig"].map(function(i) {return(pval_a[i])}).map(Math.log10).map(function(x) {return(-x)}),
-      mode: 'markers',
-      type: 'scatter',
-      color: 'red'
-    }
-    var non_signifcant_trace = {
-      x : SigObj["non_sig"].map(function(i) {return(logFC_a[i])}),
-      y : SigObj["non_sig"].map(function(i) {return(pval_a[i])}).map(Math.log10).map(function(x) {return(-x)}),
-      mode: 'markers',
-      type: 'scatter',
-      color: 'blue'
-    }
-    var traces = [significant_trace, non_signifcant_trace];
-    return traces
+    data[0].x = SigObj["sig"].map(function(i) {return(logFC_a[i])})
+    data[0].y = SigObj["sig"].map(function(i) {return(pval_a[i])}).map(Math.log10).map(function(x) {return(-x)})
+
+    data[1].x = SigObj["non_sig"].map(function(i) {return(logFC_a[i])})
+    data[1].y = SigObj["non_sig"].map(function(i) {return(pval_a[i])}).map(Math.log10).map(function(x) {return(-x)})
+
+    /// var non_signifcant_trace = {
+    ///   x : SigObj["non_sig"].map(function(i) {return(logFC_a[i])}),
+    ///   y : SigObj["non_sig"].map(function(i) {return(pval_a[i])}).map(Math.log10).map(function(x) {return(-x)}),
+    ///   mode: 'markers',
+    ///   type: 'scatter',
+    ///   color: 'blue'
+    /// }
+    return data;
     //traces.push()
   },
   draw: function(){
+    var _this = this; 
+    console.log("inside draw")
+    console.log(this.project)
+    //console.log(_this.project.getFullDataset());
+    console.log("loaded")
     var plotlyDefaults = phantasus.volcanoTool.getPlotlyDefaults();
     var layout = plotlyDefaults.layout;
     var config = plotlyDefaults.config;
     console.log("inside draw")
     var myPlot = this.$chart[0];
-    var traces = this.annotate()
+    var traces = _this.annotate()
     //console.log(myPlot);
     return(phantasus.volcanoTool.newPlot(myPlot, traces, layout, config));
   }
