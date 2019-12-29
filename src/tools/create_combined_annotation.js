1690ead968b0a5e8065302cb3f151be66c70c7f4
Saksham Malhotra <saksham2196@gmail.com>
2019-12-29 17:42:24 +0530

    lint

--- src/tools/create_combined_annotation.js
+++ src/tools/create_combined_annotation.js
@@ -1,72 +1,78 @@
-phantasus.CreateCombinedAnnotation = function (project) {
-};
+phantasus.CreateCombinedAnnotation = function(project) {};
 phantasus.CreateCombinedAnnotation.prototype = {
-  toString: function () {
-    return 'Create Combined Annotation';
+  toString: function() {
+    return "Create Combined Annotation";
   },
-  gui: function () {
-    return [{
-      name: 'annotate',
-      options: ['Columns', 'Rows'],
-      value: 'Rows',
-      type: 'radio'
-    },{
-      name: 'annotation1',
-      type: 'select'
-    }, {
-      name: 'annotation2',
-      type: 'select'
-    }, {
-      name: 'separator',
-      type: 'text',
-      value: '_'
-    }, {
-      name: 'annotation_name',
-      value: '',
-      type: 'text',
-      help: 'Optional annotation name. If not specified, the operation name will be used.',
-      autocomplete: 'off'
-    }, {
-      name: 'use_selected_rows_and_columns_only',
-      type: 'checkbox'
-    }];
+  gui: function() {
+    return [
+      {
+        name: "annotate",
+        options: ["Columns", "Rows"],
+        value: "Rows",
+        type: "radio"
+      },
+      {
+        name: "annotation1",
+        type: "select"
+      },
+      {
+        name: "annotation2",
+        type: "select"
+      },
+      {
+        name: "separator",
+        type: "text",
+        value: "_"
+      },
+      {
+        name: "annotation_name",
+        value: "",
+        type: "text",
+        help:
+          "Optional annotation name. If not specified, the operation name will be used.",
+        autocomplete: "off"
+      },
+      {
+        name: "use_selected_rows_and_columns_only",
+        type: "checkbox"
+      }
+    ];
   },
-  init: function(project, form){
-    var setValue = function (val) {
-      var isRows = val === 'Rows';
-      var names = phantasus
-        .MetadataUtil
-        .getMetadataNames(
-          isRows ?
-            project.getFullDataset().getRowMetadata() :
-            project.getFullDataset().getColumnMetadata());
-      
-      form.setOptions('annotation1', names);
-      form.setValue('annotation1', names[0]);
-      form.setOptions('annotation2', names);
-      form.setValue('annotation2', names[0]);
+  init: function(project, form) {
+    var setValue = function(val) {
+      var isRows = val === "Rows";
+      var names = phantasus.MetadataUtil.getMetadataNames(
+        isRows
+          ? project.getFullDataset().getRowMetadata()
+          : project.getFullDataset().getColumnMetadata()
+      );
+
+      form.setOptions("annotation1", names);
+      form.setValue("annotation1", names[0]);
+      form.setOptions("annotation2", names);
+      form.setValue("annotation2", names[0]);
     };
 
-    form.$form.find('[name=annotate]').on('change', function (e) {
+    form.$form.find("[name=annotate]").on("change", function(e) {
       setValue($(this).val());
     });
 
-    setValue('Rows');
-
+    setValue("Rows");
   },
-  execute: function (options) {
+  execute: function(options) {
     var project = options.project;
     var selectedOnly = options.input.use_selected_rows_and_columns_only;
-    var isColumns = options.input.annotate === 'Columns';
+    var isColumns = options.input.annotate === "Columns";
     var annotation1 = options.input.annotation1;
     var annotation2 = options.input.annotation2;
     var separator = options.input.separator;
-    var annotationName = options.input.annotation_name || (annotation1 + separator + annotation2);
+    var annotationName =
+      options.input.annotation_name || annotation1 + separator + annotation2;
     var dataset = selectedOnly
       ? project.getSelectedDataset({
-        selectedRows: true,
-        selectedColumns: true,
-      })
+          selectedRows: true,
+          selectedColumns: true
+        })
       : project.getFullDataset();
 
     if (isColumns) {
@@ -74,7 +80,6 @@ phantasus.CreateCombinedAnnotation.prototype = {
     }
 
     var rowView = new phantasus.DatasetRowView(dataset);
-    console.log(rowView);
     var vector = dataset.getRowMetadata().add(annotationName);
 
     var idx = 0;
@@ -83,13 +88,16 @@ phantasus.CreateCombinedAnnotation.prototype = {
     var annotation2Vector = dataset.getRowMetadata().getByName(annotation2);
     for (var size = dataset.getRowCount(); idx < size; idx++) {
       rowView.setIndex(idx);
-      var val = annotation1Vector.getValue(idx) + separator + annotation2Vector.getValue(idx);
+      var val =
+        annotation1Vector.getValue(idx) +
+        separator +
+        annotation2Vector.getValue(idx);
       vector.setValue(idx, val);
     }
 
-    project.trigger('trackChanged', {
+    project.trigger("trackChanged", {
       vectors: [vector],
-      display: isColumns? ['color']:['text'],
+      display: isColumns ? ["color"] : ["text"],
       columns: isColumns
     });
   }
