8e69bf5e1db1a28ccf66778e3de3254454cdab35
Alexey Sergushichev <alsergbox@gmail.com>
2023-06-26 14:56:26 -0500

    update to google analytics 4

--- src/util/util.js
+++ src/util/util.js
@@ -46,23 +46,22 @@ phantasus.Util.loadTrackingCode = function () {
   if (phantasus.Util.TRACKING_ENABLED && typeof window !== 'undefined' && typeof navigator !== 'undefined' && navigator.onLine) {
     if (phantasus.Util.TRACKING_CODE_LOADED) {
       return;
-    } else if (typeof ga === 'undefined') {
+    } else {
+        console.log("loading analytics");
       phantasus.Util.TRACKING_CODE_LOADED = true;
-      (function (i, s, o, g, r, a, m) {
-        i['GoogleAnalyticsObject'] = r;
-        i[r] = i[r] || function () {
-          (i[r].q = i[r].q || []).push(arguments);
-        }, i[r].l = 1 * new Date();
-        a = s.createElement(o),
-          m = s.getElementsByTagName(o)[0];
-        a.async = 1;
-        a.src = g;
-        m.parentNode.insertBefore(a, m);
-      })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
-    }
-    if (typeof ga !== 'undefined') {
-      ga('create', 'UA-53973555-1', 'auto', 'phantasus');
-      ga('phantasus.send', 'pageview');
+
+      var a = document.createElement('script');
+      var m = document.getElementsByTagName('script')[0];
+      a.async = 1;
+      a.src = 'https://www.googletagmanager.com/gtag/js?id=G-727Q0ZZHXL';
+      m.parentNode.insertBefore(a, m);
+        console.log("loaded?");
+
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-727Q0ZZHXL');
+      
     }
     phantasus.Util.TRACKING_CODE_LOADED = true;
   }
